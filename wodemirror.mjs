9e2c084ffc161877686fce86c1a187d7accb39fc
Matt Mundell <matt@mundell.me>
2024-09-30 14:16:52 +0200

    highlightSpecials

--- wodemirror.mjs
+++ wodemirror.mjs
@@ -509,6 +509,7 @@ function viewInit
   view.wode.decorMode = new CMState.Compartment
   view.wode.drawSelection = new CMState.Compartment
   view.wode.highlightActive = new CMState.Compartment
+  view.wode.highlightSpecials = new CMState.Compartment
   view.wode.highlightSyntax = new CMState.Compartment
   view.wode.highlightWhitespace = new CMState.Compartment
   view.wode.showTrailingWhitespace = new CMState.Compartment
@@ -713,6 +714,11 @@ function viewInit
     opts.push(view.wode.highlightActive.of(CMView.highlightActiveLine()))
   else
     opts.push(view.wode.highlightActive.of([]))
+
+  if (buf.opt('core.highlight.specials.enabled'))
+    opts.push(view.wode.highlightSpecials.of(CMView.highlightSpecialChars()))
+  else
+    opts.push(view.wode.highlightSpecials.of([]))
 
   if (buf.opt('core.highlight.syntax.enabled')) {
     opts.push(view.wode.highlightSyntax.of(makeHighlightSyntax()))
@@ -3774,6 +3780,14 @@ function reconfigureHighlightActive
     view.ed.dispatch({ effects: view.wode.highlightActive.reconfigure([]) })
 }
 
+function reconfigureHighlightSpecials
+(buf, view) {
+  if (buf.opt('core.highlight.specials.enabled'))
+    view.ed.dispatch({ effects: view.wode.highlightSpecials.reconfigure(CMView.highlightSpecialChars()) })
+  else
+    view.ed.dispatch({ effects: view.wode.highlightSpecials.reconfigure([]) })
+}
+
 function reconfigureHighlightTrailing
 (buf, view) {
   if (buf.opt('core.highlight.trailingWhitespace.enabled'))
@@ -3861,6 +3875,7 @@ function initOpt
 
   on('core.autocomplete', reconfigureAutocomplete)
   on('core.highlight.activeLine.enabled', reconfigureHighlightActive)
+  on('core.highlight.specials.enabled', reconfigureHighlightSpecials)
   on('core.highlight.syntax.enabled', reconfigureHighlightSyntax)
   on('core.highlight.trailingWhitespace.enabled', reconfigureHighlightTrailing)
   on('core.highlight.whitespace.enabled', reconfigureHighlightWhitespace)

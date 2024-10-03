9bfec9afe54c436b1f2d11f07ead8dd151261eed
Matt Mundell <matt@mundell.me>
2024-10-03 15:00:06 +0200

    test ext.canon

---
+++ test/ext.mjs
@@ -0,0 +1,29 @@
+import { equal } from 'node:assert/strict'
+import * as Ext from '../ext.mjs'
+
+let tests, canon
+
+function test
+(group, name, cb) {
+  tests[group] = tests[group] || []
+  tests[group].push({ name: name, cb: cb })
+}
+
+tests = {}
+canon = Ext._internals.canon
+
+test('canon', 'core',
+     () => equal(canon('core'),
+                 'core'))
+
+test('canon', 'blankLines',
+     () => equal(canon('blankLines'),
+                 'blank-lines'))
+
+test('canon', 'co/re1_23-eg.mjs',
+     () => equal(canon('co/re1_23-eg.mjs'),
+                 'co/re1_23-eg.mjs'))
+
+Object.entries(tests).forEach(group => globalThis.describe(group[0],
+                                                           () => group[1].forEach(t => globalThis.it(t.name,
+                                                                                                     t.cb))))

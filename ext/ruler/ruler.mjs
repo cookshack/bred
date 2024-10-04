54f97c7ce86b104c3d852d2ce3061104fd60c109
Matt Mundell <matt@mundell.me>
2024-10-04 10:43:48 +0200

    ruler opt

--- ext/ruler/ruler.mjs
+++ ext/ruler/ruler.mjs
@@ -1,73 +1,45 @@
-import * as Buf from '../../buf.mjs'
 import * as Cmd from '../../cmd.mjs'
-import * as Mode from '../../mode.mjs'
+import * as Ed from '../../ed.mjs'
 import * as Opt from '../../opt.mjs'
-import * as Pane from '../../pane.mjs'
 //import { d } from '../../mess.mjs'
 
 import * as Ruler from './lib/@cookshack/codemirror-ruler.js'
+import * as CMState from '../../lib/@codemirror/state.js'
+
+let brexts
 
 export
 function init
 () {
-  let mode
-
-  Opt.declare('ruler.col', 'int', 50)
-  mode = Mode.add('Ruler', { minor: 1 })
-
-  Cmd.add('ruler mode', () => {
-    let p, ruler, exts
-
-    p = Pane.current()
-
-    if (p.buf.toggleMode(mode))
-      p.buf.views.forEach(view => {
-        if (view.ed) {
-          ruler = Ruler.make(view.ed, { col: p.buf.opt('ruler.col') })
-          view.vars('Ruler').ruler = ruler
-          exts = ruler.exts.flat(Infinity)
-          exts.forEach(e => view.addExt({ cm: e }))
-        }
-      })
-    else
-      p.buf.views.forEach(view => {
-        if (view.ed) {
-          ruler = view.vars('Ruler').ruler
-          if (ruler) {
-            exts = ruler.exts.flat(Infinity)
-            exts.forEach(e => view.removeExt({ cm: e }))
-          }
-        }
-      })
-  })
-
-  Opt.onSet('ruler.col', val => {
-    Buf.forEach(buf => {
-      if (buf.opts.get('ruler.col') === undefined)
-        buf.views.forEach(view => {
-          let ruler
-
-          ruler = view.vars('Ruler').ruler
-          if (ruler)
-            ruler.set('col', val)
-        })
-    })
-  })
-
-  Opt.onSetBuf('ruler.col', (buf, val) => {
-    buf.views.forEach(view => {
+  function make
+  (view) {
+    if (view.buf.opt('ruler.enabled')) {
       let ruler
 
-      ruler = view.vars('Ruler').ruler
+      ruler = Ruler.make({ col: view.buf.opt('ruler.col') })
       if (ruler)
-        ruler.set('col', val)
-    })
-  })
+        return ruler.exts
+    }
+    return []
+  }
+
+  brexts = []
+  Opt.declare('ruler.enabled', 'bool', 1)
+  Opt.declare('ruler.col', 'int', 100)
+
+  brexts.push(Ed.register({ backend: 'cm',
+                            make,
+                            part: new CMState.Compartment,
+                            reconfOpts: [ 'ruler.enabled', 'ruler.col' ] }))
+
+  Cmd.add('enable ruler', u => Ed.enable(u, 'ruler.enabled'))
+  Cmd.add('buffer enable ruler', u => Ed.enableBuf(u, 'ruler.enabled'))
 }
 
 export
 function free
 () {
-  Mode.remove('Ruler')
-  Cmd.remove('ruler mode')
+  Cmd.remove('enable ruler')
+  Cmd.remove('buffer enable ruler')
+  brexts.forEach(b => b?.free())
 }

0bec8dadd975ea282a16cadd39484345634d4c40
Matt Mundell <matt@mundell.me>
2024-10-16 15:16:36 +0200

    zebra opt

--- ext/zebra/zebra.mjs
+++ ext/zebra/zebra.mjs
@@ -1,29 +1,32 @@
 import * as Cmd from '../../cmd.mjs'
-import * as Mode from '../../mode.mjs'
-import * as Pane from '../../pane.mjs'
+import * as Ed from '../../ed.mjs'
+import * as Opt from '../../opt.mjs'
 //import { d } from '../../mess.mjs'
 
 import { zebraStripes } from './lib/@cookshack/codemirror-zebra.js'
+import * as CMState from '../../lib/@codemirror/state.js'
+
+let brexts
 
 export
 function init
 () {
-  let mode
+  brexts = []
+  Opt.declare('zebra.enabled', 'bool', 0)
 
-  mode = Mode.add('Zebra', { minor: 1 })
+  brexts.push(Ed.register({ backend: 'cm',
+                            make: view => view.buf.opt('zebra.enabled') ? zebraStripes() : [],
+                            part: new CMState.Compartment,
+                            reconfOpts: [ 'zebra.enabled' ] }))
 
-  Cmd.add('zebra mode', () => {
-    let p
-
-    p = Pane.current()
-    p?.buf.addExt({ cm: zebraStripes() })
-    p?.buf.addMode(mode)
-  })
+  Cmd.add('enable zebra', u => Ed.enable(u, 'zebra.enabled'))
+  Cmd.add('buffer enable zebra', u => Ed.enableBuf(u, 'zebra.enabled'))
 }
 
 export
 function free
 () {
-  Mode.remove('Zebra')
-  Cmd.remove('zebra mode')
+  Cmd.remove('enable zebra')
+  Cmd.remove('buffer enable zebra')
+  brexts.forEach(b => b?.free())
 }

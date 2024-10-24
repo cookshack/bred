d54e6a0593bf20eb0825fdd82988656f5f9bdaeb
Matt Mundell <matt@mundell.me>
2024-10-24 16:23:54 +0200

    core.search.files.recurse

--- dir.mjs
+++ dir.mjs
@@ -693,7 +693,7 @@ function initSearchFiles
     if (needle && needle.length) {
       hist.add(needle)
       // find . -type f -not -name \*.BAK -not -name \*.CKP -not -name \*~ -maxdepth 1 2>/dev/null | xargs grep --ignore-case --fixed-strings --line-number "$1" -H -I 2>/dev/null # -I -e -H 2>/dev/null
-      Shell.spawn1(Loc.appDir().join('bin/sr'), 1, 1, [ needle ], 0, b => {
+      Shell.spawn1(Loc.appDir().join('bin/sr'), 1, 1, [ needle, p.buf.opt('core.search.files.recurse') ? '1' : '0' ], 0, b => {
         b.mode = 'sr'
         b.addMode('view')
       })

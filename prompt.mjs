1a018464e6e599a9c240b0fcc3fd209ab14f2901
Matt Mundell <matt@mundell.me>
2024-11-06 18:35:50 +0200

    fix place num

--- prompt.mjs
+++ prompt.mjs
@@ -122,7 +122,7 @@ function promptBuf
                     modeName: 'Prompt2',
                     content: spec.w,
                     dir: p.dir,
-                    placeholder: spec.hist?.nth(0) })
+                    placeholder: spec.hist?.nth(0)?.toString() })
   buf.vars('ed').fillParent = 0
   buf.opts.set('core.autocomplete.enabled', 0)
   buf.opts.set('core.folding.enabled', 0)

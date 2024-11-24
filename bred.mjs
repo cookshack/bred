47477fc77a8ed81c53492e9cc064d90811639122
Matt Mundell <matt@mundell.me>
2024-11-24 15:56:41 +0200

    ext query

--- bred.mjs
+++ bred.mjs
@@ -1225,185 +1225,6 @@ function initBrowse
   })
 }
 
-function initLlm
-() {
-  let hist, mo
-
-  function divW
-  (query) {
-    return divCl('query-ww',
-                 [ divCl('query-h', 'Query: ' + query),
-                   divCl('query-w', 'Fetching...') ])
-  }
-
-  function refresh
-  (view) {
-    let w, co
-
-    if (0) {
-      w = view.ele.querySelector('.query-w')
-      //w.innerHTML = 'xx'
-
-      co = recents.map(r => divCl('query-item',
-                                  r.href,
-                                  { 'data-run': 'open link',
-                                    'data-path': r.href }))
-
-      append(w, co)
-    }
-  }
-
-  function first
-  (v) {
-    v.point.put(v.ele.querySelector('.query-item'))
-  }
-
-  function next
-  (nth) {
-    let h, el, v
-
-    v = Pane.current().view
-    h = v.ele.querySelector('.query-h')
-    if (v.point.over(h)) {
-      first(v)
-      return
-    }
-    el = v.point.over()
-    if (el) {
-      let item
-
-      if (Css.has(el, 'query-item'))
-        item = el
-      else
-        item = el.closest('.query-item')
-
-      if (nth == -1)
-        el = item.previousElementSibling
-      else
-        el = item.nextElementSibling
-      if (Css.has(el, 'query-item')) {
-        v.point.put(el)
-        return
-      }
-    }
-    else
-      first(v)
-  }
-
-  function snippet
-  (item) {
-    let split, date
-
-    split = item.snippet.split(' ', 3)
-    if (split && (split.length > 2) && /[0-9][0-9][0-9][0-9]/.test(split[2]))
-      date = split.join(' ')
-    if (date)
-      return [ divCl('query-item-date', date),
-               item.snippet.slice(date.length) ]
-    return item.snippet
-  }
-
-  function title
-  (item) {
-    let favi
-
-    try {
-      let url
-
-      url = new URL(item.link)
-      favi = url.protocol + '//' + url.host + '/favicon.ico'
-    }
-    catch (err) {
-      Ed.use(err)
-    }
-
-    if (favi)
-      return [ divCl('query-item-icon',
-                     img(favi, 'Icon')),
-               item.title ]
-    return item.title
-  }
-
-  /* You are an expert. Based on the following search results, please provide a summary or answer to my question:
-
-Search Results:
-1.
-2.
-3.
-
-Question: ...
-*/
-
-  Cmd.add('llm', (u, we, model) => {
-    model = model || Opt.get('llm.model')
-    Prompt.ask({ text: 'Prompt',
-                 hist: hist },
-               prompt => {
-                 hist.add(prompt)
-                 Shell.spawn1('llm', 1, 0, [ model, prompt ], 0, buf => {
-                   buf.opts.set('core.line.wrap.enabled', 1)
-                   buf.opts.set('core.lint.enabled', 0)
-                   buf.mode = 'richdown'
-                 })
-               })
-  })
-
-  Cmd.add('google', () => {
-    Prompt.ask({ text: 'Query',
-                 hist: hist },
-               query => {
-                 let p, buf
-
-                 p = Pane.current()
-                 buf = Buf.add('Query', 'Query', divW(query), p.dir)
-                 p.setBuf(buf)
-                 hist.add(query)
-                 fetch('https://www.googleapis.com/customsearch/v1'
-                       + '?cx=' + Opt.get('google.cx')
-                       + '&key=' + Opt.get('google.key')
-                       + '&q=' + query)
-                   .then(response => {
-                     response.ok || Mess.toss(response.statusText)
-                     return response.json()
-                   })
-                   .then(data => {
-                     d(data)
-                     buf?.views.forEach(view => {
-                       if (view.ele) {
-                         let w
-
-                         w = view.ele.querySelector('.query-w')
-                         w.innerHTML = ''
-                         append(w,
-                                data.items.map(item => divCl('query-item',
-                                                             [ divCl('query-item-t',
-                                                                     title(item),
-                                                                     { 'data-run': 'open externally',
-                                                                       'data-url': item.link }),
-                                                               divCl('query-item-url',
-                                                                     item.formattedUrl),
-                                                               divCl('query-item-snippet',
-                                                                     snippet(item)) ])))
-                       }
-                     })
-                   })
-                   .catch(error => {
-                     Mess.yell(error.message)
-                   })
-               })
-  })
-
-  hist = Hist.ensure('llm')
-
-  mo = Mode.add('Query', { viewInit: refresh })
-
-  Cmd.add('next', () => next(), mo)
-  Cmd.add('previous', () => next(-1), mo)
-
-  Em.on('n', 'Next', mo)
-  Em.on('p', 'Previous', mo)
-}
-
 function initMakeDir
 () {
   function makeDir
@@ -1870,7 +1691,6 @@ function initCss
             './css/test-buffer.css',
             './css/manpage.css',
             './css/options.css',
-            './css/query.css',
             './css/recent.css',
             './css/vc.css',
             './css/web.css' ]
@@ -1882,6 +1702,7 @@ function initCss
       m.sheets.forEach(f => initCss1(f))
     },
           err => Mess.yell('Failed to load  ' + file + ': ' + err.message))
+    .catch(err => Mess.yell('Failed to import  ' + file + ': ' + err.message))
 }
 
 function scratchMessage
@@ -1965,7 +1786,6 @@ function start2
   initBrowse()
   initFile()
   initEvalLine()
-  initLlm()
 
   initRecent()
   Ext.loadAll() // async

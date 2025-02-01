89dcebd51df899b46d85384ccf0de051ce71d635
Matt Mundell <matt@mundell.me>
2025-02-01 22:30:40 +0200

    proj git

--- main-project.mjs
+++ main-project.mjs
@@ -1,19 +1,49 @@
+import { spawnSync } from 'node:child_process'
+import Path from 'node:path'
+import { d } from './main-log.mjs'
+
 let projects
 
 projects = [ { main: 1, dir: '' } ]
 
+function add
+(dir) {
+  let p
+
+  d('PROJ add ' + dir)
+
+  p = { dir }
+
+  projects.push(p)
+  return p
+}
+
 export
 function get
 (path) {
-  let p
+  let p, res
+
+  d('PROJ get ' + path)
 
   // search existing projects
-  p = projects.find(p1 => path.startsWith(p1.dir))
-  if (p)
+  p = projects.find(p1 => {
+    d('  ' + p1.dir)
+    return p1.dir.length && path.startsWith(p1.dir)
+  })
+  if (p) {
+    d('PROJ found in existing')
     return p
+  }
 
-  // search up the dir tree for git repos
+  // is it in a git repo?
+  res = spawnSync('git',
+                  [ 'rev-parse', '--show-toplevel' ],
+                  { cwd: Path.dirname(path),
+                    encoding: 'utf-8' })
+  d(res.error)
+  if (res.error)
+    // fallback to the catchall project 'main'
+    return projects[0]
 
-  // fallback to the catchall project 'main'
-  return projects[0]
+  return add(res.stdout.trim())
 }

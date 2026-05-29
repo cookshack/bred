531fe4859f8f6b958bac18e588d2910911f63700
Matt Mundell <matt@mundell.me>
2026-05-29 18:26:05 +0200

    patch refine corners,test

--- js/patch.mjs
+++ js/patch.mjs
@@ -1,5 +1,75 @@
 import { diffChars } from '../lib/diff.js'
 
+function mapToLine
+(lines, offset) {
+  let pos
+
+  pos = 0
+  for (let i = 0; i < lines.length; i++) {
+    let len
+
+    len = lines[i].text.length
+    if (offset < pos + len)
+      return { lineNum: lines[i].lineNum, lineOff: offset - pos }
+    pos += len + 1
+  }
+  return { lineNum: lines[lines.length - 1].lineNum, lineOff: lines[lines.length - 1].text.length }
+}
+
+function diffBlock
+(results, oldLines, newLines) {
+  let oldJoined, newJoined, changes, oldCur, newCur, j
+
+  oldJoined = oldLines.map(l => l.text).join('\n')
+  newJoined = newLines.map(l => l.text).join('\n')
+  changes = diffChars(oldJoined, newJoined)
+  oldCur = 0
+  newCur = 0
+
+  for (j = 0; j < changes.length; j++) {
+    let ch, segments, s
+
+    ch = changes[j]
+    segments = ch.value.split('\n')
+    for (s = 0; s < segments.length; s++) {
+      let seg, segLen
+
+      seg = segments[s]
+      segLen = seg.length
+
+      if (ch.removed) {
+        if (segLen) {
+          let mapped
+
+          mapped = mapToLine(oldLines, oldCur)
+          results.push({ line: mapped.lineNum,
+                         from: mapped.lineOff + 1,
+                         to: mapped.lineOff + 1 + segLen,
+                         type: '-' })
+        }
+        oldCur += segLen + (s < segments.length - 1 ? 1 : 0)
+      }
+      else if (ch.added) {
+        if (segLen) {
+          let mapped
+
+          mapped = mapToLine(newLines, newCur)
+          results.push({ line: mapped.lineNum,
+                         from: mapped.lineOff + 1,
+                         to: mapped.lineOff + 1 + segLen,
+                         type: '+' })
+        }
+        newCur += segLen + (s < segments.length - 1 ? 1 : 0)
+      }
+      else {
+        oldCur += segLen + (s < segments.length - 1 ? 1 : 0)
+        newCur += segLen + (s < segments.length - 1 ? 1 : 0)
+      }
+    }
+  }
+}
+
+export
 function computeRefine
 (patch) {
   let lines, results, i
@@ -65,46 +135,9 @@ function computeRefine
               break
           }
 
-          {
-            let maxPairs, p
-
-            // Pair old/new lines and diff them
-            maxPairs = Math.min(oldLines.length, newLines.length)
-            for (p = 0; p < maxPairs; p++) {
-              let changes, oldPos, newPos, j
-
-              changes = diffChars(oldLines[p].text, newLines[p].text)
-              oldPos = 1 // skip '-'
-              newPos = 1 // skip '+'
-
-              for (j = 0; j < changes.length; j++) {
-                let ch
-
-                ch = changes[j]
-                if (ch.removed) {
-                  let from, to
-
-                  from = oldPos
-                  to = oldPos + ch.value.length
-                  results.push({ line: oldLines[p].lineNum, from, to, type: '-' })
-                  oldPos = to
-                }
-                else if (ch.added) {
-                  let from, to
-
-                  from = newPos
-                  to = newPos + ch.value.length
-                  results.push({ line: newLines[p].lineNum, from, to, type: '+' })
-                  newPos = to
-                }
-                else {
-                  oldPos += ch.value.length
-                  newPos += ch.value.length
-                }
-              }
-            }
-            // Unpaired lines (extra - or +) get no refine
-          }
+          if (oldLines.length && newLines.length && oldLines.length == newLines.length)
+            diffBlock(results, oldLines, newLines)
+          // Unpaired lines (extra - or +) get no refine
           continue
         }
 
---
+++ test/patch.mjs
@@ -0,0 +1,113 @@
+import { deepStrictEqual } from 'node:assert/strict'
+
+import { computeRefine } from '../js/patch.mjs'
+
+let tests
+
+tests = {}
+
+function test
+(group, name, cb) {
+  tests[group] = tests[group] || []
+  tests[group].push({ name, cb })
+}
+
+function p
+(oldLines, newLines) {
+  let head
+
+  head = '--- a/file\n+++ b/file\n@@ -1,' + oldLines.length + ' +1,' + newLines.length + ' @@\n'
+  return head + oldLines.map(l => '-' + l).join('\n') + '\n' + newLines.map(l => '+' + l).join('\n') + '\n'
+}
+
+test('single line', 'no change', () => {
+                                   deepStrictEqual(computeRefine(p([ 'hello' ], [ 'hello' ])), [])
+                                 })
+
+test('single line', 'full change', () => {
+                                     deepStrictEqual(computeRefine(p([ 'abc' ], [ 'xyz' ])),
+                                                     [ { line: 4, from: 1, to: 4, type: '-' },
+                                                       { line: 5, from: 1, to: 4, type: '+' } ])
+                                   })
+
+test('single line', 'partial change at start', () => {
+                                                 deepStrictEqual(computeRefine(p([ 'abcdef' ], [ 'xyzdef' ])),
+                                                                 [ { line: 4, from: 1, to: 4, type: '-' },
+                                                                   { line: 5, from: 1, to: 4, type: '+' } ])
+                                               })
+
+test('single line', 'partial change at middle', () => {
+                                                  deepStrictEqual(computeRefine(p([ 'abcdef' ], [ 'abxyzf' ])),
+                                                                  [ { line: 4, from: 3, to: 6, type: '-' },
+                                                                    { line: 5, from: 3, to: 6, type: '+' } ])
+                                                })
+
+test('single line', 'partial change at end', () => {
+                                               deepStrictEqual(computeRefine(p([ 'abcdef' ], [ 'abcxyz' ])),
+                                                               [ { line: 4, from: 4, to: 7, type: '-' },
+                                                                 { line: 5, from: 4, to: 7, type: '+' } ])
+                                             })
+
+test('single line', 'adjacent changes', () => {
+                                          deepStrictEqual(computeRefine(p([ 'ab' ], [ 'cd' ])),
+                                                          [ { line: 4, from: 1, to: 3, type: '-' },
+                                                            { line: 5, from: 1, to: 3, type: '+' } ])
+                                        })
+
+test('multi line', 'reorder two lines', () => {
+                                          let result
+
+                                          result = computeRefine(p([ 'foo(world)', 'foo(hello)' ],
+                                                                   [ 'foo(hello)', 'foo(world)' ]))
+                                          // Both lines have word swaps detected
+                                          deepStrictEqual(result.length, 7)
+                                        })
+
+test('multi line', 'partial change across lines', () => {
+                                                    deepStrictEqual(computeRefine(p([ 'alpha', 'beta' ],
+                                                                                    [ 'aloha', 'beta' ])),
+                                                                    [ { line: 4, from: 3, to: 4, type: '-' },
+                                                                      { line: 6, from: 3, to: 4, type: '+' } ])
+                                                  })
+
+test('multi line', 'unbalanced fewer old', () => {
+                                             deepStrictEqual(computeRefine(p([ 'abc' ],
+                                                                             [ 'abc', 'extra' ])), [])
+                                           })
+
+test('multi line', 'unbalanced fewer new', () => {
+                                             deepStrictEqual(computeRefine(p([ 'abc', 'extra' ],
+                                                                             [ 'abc' ])), [])
+                                           })
+
+test('empty', 'empty patch', () => {
+                               deepStrictEqual(computeRefine(''), [])
+                             })
+
+test('empty', 'no hunks', () => {
+                            deepStrictEqual(computeRefine('context only\nno diff\n'), [])
+                          })
+
+test('empty', 'hunk without changes', () => {
+                                        deepStrictEqual(computeRefine('--- a\n+++ b\n@@ -1,1 +1,1 @@\n context\n'), [])
+                                      })
+
+test('multiple hunks', 'two hunks', () => {
+                                      deepStrictEqual(computeRefine('--- a\n+++ b\n@@ -1,1 +1,1 @@\n-abc\n+xyz\n@@ -2,1 +2,1 @@\n-def\n+uvw\n'),
+                                                      [ { line: 4, from: 1, to: 4, type: '-' },
+                                                        { line: 5, from: 1, to: 4, type: '+' },
+                                                        { line: 7, from: 1, to: 4, type: '-' },
+                                                        { line: 8, from: 1, to: 4, type: '+' } ])
+                                    })
+
+test('pure addition', 'extra + line', () => {
+                                        deepStrictEqual(computeRefine(p([ 'old' ], [ 'old', 'new' ])), [])
+                                      })
+
+test('pure removal', 'extra - line', () => {
+                                       deepStrictEqual(computeRefine(p([ 'old', 'gone' ], [ 'old' ])), [])
+                                     })
+
+Object.entries(tests).forEach(group => globalThis.describe(group[0],
+                                                           () => group[1].forEach(t => globalThis.it(t.name,
+                                                                                                     t.cb))))
